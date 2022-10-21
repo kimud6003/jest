513205c20cf8a4c0ad360fec97df1147e32bc780
KUD <kimud6003@gmail.com>
2022-10-21 21:46:09 +0900

    React-jest 위한 삭제

--- __test__/listMaker.js
+++
@@ -1,9 +0,0 @@
-function listMaker(index= 3){
-  let list =[];
-
-  for(let i=1;i<=index;i++){
-    list.push(i);
-  }
-  return list
-}
-export default listMaker;
--- __test__/listUser.js
+++
@@ -1,7 +0,0 @@
-import listMaker from "./listMaker";
-
-function listUser(){
-  return listMaker(5);
-}
-
-export default listUser;
